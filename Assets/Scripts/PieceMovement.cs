c91f58977e7c71d5d3cd607480e3430a71797978
Jerod <jerodpeterson2@gmail.com>
2022-10-05 15:21:14 -0500

    jail and ignored spaces implemented

--- Assets/Scripts/PieceMovement.cs
+++ Assets/Scripts/PieceMovement.cs
@@ -8,16 +8,25 @@ public class PieceMovement : MonoBehaviour
     public GameObject piece;
     private List<Transform> positions;
 
-    public float moveTime = 0.4f;
-    public float jumpHeight = 3;
+    public float moveTime = 0.3f;
+    public float jumpHeight = 1;
     public int targetSpace = 0;
     public int moveSpacesCount = 0;
     public bool directJump = false;
     public bool jumpCounter = false;
+    public Vector3 offset;
+
+    public bool jailTime = false;
+    public bool noJail = false;
 
     //dont modify
     public int currentSpace = 0;
-    
+
+    private List<int> ignorePos = new List<int> { 41 };
+    private int visitNum = 10;
+    private int jailNum = 11;
+    private Transform jail;
+
     private int nextSpace = 0;
     private int spaces;
     //public Transform pos;
@@ -30,12 +39,36 @@ public class PieceMovement : MonoBehaviour
     private Vector2 moveVector;
     private float parabolaHeight;
     private bool moving = false;
-    
+    private bool inJail = false;
+
+
+    public void goToJail()
+    {
+        inJail = true;
+        jumpCounter = false;
+        directJump = true;
+        //just make it so the piece has to move
+        targetSpace = -1;
+        nextSpace = -1;
+
+        sp1 = piece.transform.position;
+        sp2 = jail.position + offset;
+        sp1xz = new Vector2(sp1.x, sp1.z);
+        sp2xz = new Vector2(sp2.x, sp2.z);
+        moveVector = sp2xz - sp1xz;
+        parabolaHeight = Mathf.Max(sp1.y, sp2.y) + jumpHeight;
+        moving = true;
+    }
+
+    public void getOutOfJail()
+    {
+        directJumpTo(visitNum);
+    }
 
     public void directJumpTo(int space)
     {
-        space = space - 1;
-        if (space > spaces)
+        //space = space - 1;
+        if (space > spaces || space < 0)
             return;
 
         jumpCounter = false;
@@ -45,8 +78,8 @@ public class PieceMovement : MonoBehaviour
 
     public void moveTo(int space)
     {
-        space = space - 1;
-        if (space > spaces)
+        //space = space - 1;
+        if (space > spaces || space < 0)
             return;
 
         jumpCounter = false;
@@ -60,9 +93,23 @@ public class PieceMovement : MonoBehaviour
         moveSpacesCount = num;
     }
 
+
     int mod(int x, int m)
     {
         return (x % m + m) % m;
+    }
+
+    void InitializeInterpolation()
+    {
+        //sp1 = positions[currentSpace].position + offset;
+        sp1 = piece.transform.position;
+        sp2 = positions[nextSpace].position + offset;
+        sp1xz = new Vector2(sp1.x, sp1.z);
+        sp2xz = new Vector2(sp2.x, sp2.z);
+        moveVector = sp2xz - sp1xz;
+        parabolaHeight = Mathf.Max(sp1.y, sp2.y) + jumpHeight;
+        moving = true;
+        return;
     }
 
     void InterpolateMovement()
@@ -98,6 +145,7 @@ public class PieceMovement : MonoBehaviour
 
         piece.transform.position = new Vector3(xzMovement.x + pos.x, objectHeight, xzMovement.y + pos.z);
 
+        //movement is complete
         if (progressNum >= 1f)
         {
             piece.transform.position = sp2;
@@ -118,6 +166,17 @@ public class PieceMovement : MonoBehaviour
         //remove parent
         positions.RemoveAt(0);
 
+        //remove all spaces in ignorePos
+        ignorePos.Sort();
+        ignorePos.Reverse();
+        foreach (int i in ignorePos)
+        {
+            positions.RemoveAt(i);
+        }
+
+        jail = positions[jailNum];
+        positions.RemoveAt(jailNum);
+
         spaces = positions.Count;
 
         foreach (Transform i in positions)
@@ -125,60 +184,81 @@ public class PieceMovement : MonoBehaviour
             Debug.Log(i.position);
         }
 
-        piece.transform.position = positions[0].position;
+        piece.transform.position = positions[0].position + offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpCounter)
+        //remove this
+        if (jailTime)
         {
-            if (moveSpacesCount != 0)
+            goToJail();
+            jailTime = false;
+        }
+
+        if (noJail)
+        {
+            getOutOfJail();
+            noJail = false;
+        }
+
+        //check if piece needs to start moving
+        if (!moving)
+        {
+            //relative movement
+            if (jumpCounter)
             {
-                if (!moving)
+                if (moveSpacesCount != 0)
                 {
                     if (moveSpacesCount > 0)
                         nextSpace = (currentSpace + 1) % spaces;
                     else if (moveSpacesCount < 0)
                         nextSpace = mod((currentSpace - 1), spaces);
 
-                    sp1 = positions[currentSpace].position;
-                    sp2 = positions[nextSpace].position;
-                    sp1xz = new Vector2(sp1.x, sp1.z);
-                    sp2xz = new Vector2(sp2.x, sp2.z);
-                    moveVector = sp2xz - sp1xz;
-                    parabolaHeight = Mathf.Max(sp1.y, sp2.y) + jumpHeight;
-                    moving = true;
-                }
-                InterpolateMovement();
-            }
-        }
-        else
-        {
-            if (currentSpace != targetSpace)
-            {
-                if (targetSpace < 0 || targetSpace > spaces - 1)
-                    targetSpace = currentSpace;
-                else
-                {
-                    if (!moving)
+                    if (inJail)
                     {
-                        if (directJump)
-                            nextSpace = targetSpace;
-                        else
-                            nextSpace = (nextSpace + 1) % spaces;
-
-                        sp1 = positions[currentSpace].position;
-                        sp2 = positions[nextSpace].position;
-                        sp1xz = new Vector2(sp1.x, sp1.z);
-                        sp2xz = new Vector2(sp2.x, sp2.z);
-                        moveVector = sp2xz - sp1xz;
-                        parabolaHeight = Mathf.Max(sp1.y, sp2.y) + jumpHeight;
-                        moving = true;
+                        nextSpace = visitNum;
+                        inJail = false;
                     }
-                    InterpolateMovement();
+
+                    InitializeInterpolation();
+                }
+            }
+            //absolute movement
+            else
+            {
+                if (currentSpace != targetSpace)
+                {
+                    //can be -1 for jail
+                    if (targetSpace < -1 || targetSpace > spaces - 1)
+                        targetSpace = currentSpace;
+
+                    if (directJump)
+                    {
+                        nextSpace = targetSpace;
+                        inJail = false;
+                    }
+                    else
+                    {
+                        if (inJail)
+                        {
+                            nextSpace = visitNum;
+                            inJail = false;
+                        }
+                        else
+                            nextSpace = (currentSpace + 1) % spaces;
+                    }
+                        
+                    InitializeInterpolation();
                 }
             }
         }
+
+        if (moving)
+        {
+            InterpolateMovement();
+        }
+
     }
 }
