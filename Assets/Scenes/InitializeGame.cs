d1433025395a6e7179219729a5df789cc51fe355
mat-px <madprzez@gmail.com>
2022-11-06 15:24:05 -0500

    updated game controller

--- Assets/Scenes/InitializeGame.cs
+++ Assets/Scenes/InitializeGame.cs
@@ -7,9 +7,9 @@ public class InitializeGame
     public static int PlayerNum = 0;
     public static float GameSpeed = 0.0f;
     public static bool Quickplay = false;
-    public static bool Player1AI = true;
-    public static bool Player2AI = true;
-    public static bool Player3AI = true;
-    public static bool Player4AI = true;
+    public static bool Player1AI = false;
+    public static bool Player2AI = false;
+    public static bool Player3AI = false;
+    public static bool Player4AI = false;
     public static float GameVolume = 1.0f;
 }
