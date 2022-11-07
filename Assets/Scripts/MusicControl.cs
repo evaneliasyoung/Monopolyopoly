c73fe420ac9ffdf9fc0a7a3be458e6624d8593a8
Jerod <jerodpeterson2@gmail.com>
2022-11-06 18:26:29 -0600

    volume fix

--- Assets/Scripts/MusicControl.cs
+++ Assets/Scripts/MusicControl.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class MusicControl : MonoBehaviour
 {
+    public static MusicControl Instance;
+    public AudioSource effectSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        effectSource.volume = InitializeGame.GameVolume;
     }
 
     // Update is called once per frame
